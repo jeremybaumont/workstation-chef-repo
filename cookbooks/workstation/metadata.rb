ea69b671bace911196233b8035394967ced48cc1
Jeremy Baumont <jeremy@baumont.org>
2015-01-17 17:38:40 +0100

    Added gemrc configuration file

--- cookbooks/workstation/metadata.rb
+++ cookbooks/workstation/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "opensource@housepub.org"
 license          "Apache 2.0"
 description      "Configures Mac OS X and Linux workstations"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.2"
+version          "1.2.3"
 
 %w{ dmg homebrew }.each do |cb|
   depends cb
